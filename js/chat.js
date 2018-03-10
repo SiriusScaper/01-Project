7cd546f1941f029040b5f4db751c85735f814c83
SiriusScaper <siriusscaper@gmail.com>
2018-03-10 12:43:37 -0800

    New file for firebase call and structure init

---
+++ js/chat.js
@@ -0,0 +1,20 @@
+firebase.auth().onAuthStateChanged(function(user) {
+	// Once authenticated, instantiate Firechat with the logged in user
+	if (user) {
+		initChat(user);
+	}
+});
+
+function initChat(user) {
+	// Get a Firebase Database ref
+	var chatRef = firebase.database().ref('chat');
+
+	// Create a Firechat instance
+	var chat = new FirechatUI(
+		chatRef,
+		document.getElementById('firechat-wrapper')
+	);
+
+	// Set the Firechat user
+	chat.setUser(user.uid, user.displayName);
+}
